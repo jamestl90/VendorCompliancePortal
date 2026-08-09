b04e84c2f60f9a8d52c72013266db08a67e6975e
James L <10925058+jamestl90@users.noreply.github.com>
2026-08-09 22:42:54 +0800

    Add status endpoint and request logging

--- src/VendorCompliance.Web/Program.cs
+++ src/VendorCompliance.Web/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.UseHttpLogging();
+app.MapGet("/api/status", (IConfiguration config, 
+                           IHostEnvironment hostEnv, 
+                           ILogger<Program> logger) =>
+    {
+        logger.LogInformation("Test Message");
+        return Results.Ok(new
+        {
+            status = "ok",
+            application = config["Portal:Name"],
+            environment = hostEnv.EnvironmentName
+        });
+    }
+);
 app.Run();
