8e5992036994cec55b7384af3357d57736c58a95
James L <10925058+jamestl90@users.noreply.github.com>
2026-08-13 21:44:56 +0800

    Map Vendor with EF Core

--- src/VendorCompliance.Infrastructure/Persistence/VendorComplianceDbContext.cs
+++ src/VendorCompliance.Infrastructure/Persistence/VendorComplianceDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using VendorCompliance.Domain.Vendors;
 
 namespace VendorCompliance.Infrastructure.Persistence;
 
@@ -6,5 +7,17 @@ public sealed class VendorComplianceDbContext(
     DbContextOptions<VendorComplianceDbContext> options)
     : DbContext(options)
 {
-        
+    public DbSet<Vendor> Vendors => Set<Vendor>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Vendor>(vendor =>
+        {
+            vendor.ToTable("vendors");
+            vendor.HasKey(item => item.Id);
+            vendor.Property(item => item.Id).ValueGeneratedNever();
+            vendor.Property(item => item.Name).HasMaxLength(200).IsRequired();
+            vendor.Ignore(item => item.Documents);
+        });
+    }
 }
